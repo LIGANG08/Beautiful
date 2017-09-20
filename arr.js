de22c98f8920a2936a4ddc848702709111beaa82
Test-javascript <see@SmartestEEs-MBP.lan>
2017-09-20 15:09:38 +0800

    ligang123456

---
+++ arr.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+ 
+
+var arr =['a','b'];
+var a = 'a';
+var num = 1;
+/*console.log(typeof arr);
+console.log( `arr.__proto__ === Object.prototype -> ${arr.__proto__ === Object.prototype}`);
+console.log( `arr.__proto__.__proto__ === Object.prototype -> ${arr.__proto__.__proto__ === Object.prototype}`);
+console.log(typeof Array);
+console.log(`Function.prototype === Array.__proto__ -> ${Function.prototype === Array.__proto__}`);
+console.log(`Array.prototype === arr.__proto__ -> ${Array.prototype === arr.__proto__}`);*/
+
+/*console.log(typeof a);
+console.log(`a.__proto__ === Object.prototype -> ${a.__proto__=== Object.prototype}`);
+console.log(`a.__proto__.__proto__ === String.prototype -> ${a.__proto__.__proto__ === String.prototype}`);
+console.log(typeof String);
+console.log(`Function.prototype === Object.__proto__ -> ${Function.prototype === Object.__proto__}`);
+console.log(`String.prototype === a.__proto__ -> ${String.prototype === a.__proto__}`);*/
+
+console.log(typeof num);
+console.log(`num.__proto__ === Object.prototype -> ${num.__proto__ === Object.prototype}`);
+console.log(`num.__proto__.__proto__ === Object.prototype -> ${num.__proto__.__proto__ === Object.prototype}`);
+console.log(typeof Number);
+console.log(`Function.prototype === Number.__proto__ -> ${Function.prototype === Number.__proto__}`);
+console.log(Number.prototype === num.__proto__);
