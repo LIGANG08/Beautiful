30a4b33696a88cdfa6dec84e9551b03535366055
Test-javascript <see@SmartestEEs-MBP.lan>
2017-09-25 12:19:19 +0800

    t

--- egg-example/app/controller/home.js
+++ egg-example/app/controller/home.js
@@ -1,6 +1,7 @@
 /**
  * 
- * 2017-09-23
+ * 2017-09-23-24
+ * 
  */
 'use strict';
 
@@ -10,7 +11,44 @@ module.exports = app => {
       this.ctx.body = yield this.service.home.find();
     }
     * inde() {
-      this.ctx.body = 'hello!';
+      this.ctx.body = 'hello?';
+    }
+    * indexx() {
+      this.ctx.body = yield this.service.home.sina();
+    }
+    * test() {
+      const a = yield this.service.home.test();
+      const b = yield this.service.test.test();
+      this.ctx.body = a + b;
+    }
+    * chu() {
+      this.ctx.body = yield this.service.home.chunk();
+    }
+    * con() {
+      this.ctx.body = yield this.service.home.concat();
+    }
+    * cp() {
+      this.ctx.body = yield this.service.home.compact();
+    }
+    * hel() {
+      /* const c = this.ctx.helper.foo(this.ctx.request.query.a);
+      this.ctx.body = this.ctx.helper.foo(this.ctx.request.query.b) + c;*/
+      this.ctx.body = this.ctx.helper.foo('par');
+    }
+    * c() {
+      this.ctx.body = this.ctx.helper1.cat();
+    }
+    * d() {
+      this.ctx.body = this.ctx.helper1.dog();
+    }
+    * times() {
+      this.ctx.body = yield this.service.home.time();
+    }
+    * ste() {
+      this.ctx.body = yield this.service.test.stest();
+    }
+    * heltime() {
+      this.ctx.body = this.ctx.helper1.time();
     }
   }
   return HomeController;
